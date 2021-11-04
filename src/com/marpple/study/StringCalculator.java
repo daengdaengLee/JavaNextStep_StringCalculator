cbb3ca686d4ba1a73e7129c3860e7f7ccd28feed
Kunho Lee <gunho1020@gmail.com>
2021-11-04 20:37:58 +0900

    feat: 음의 정수가 포함될 때 쉼표로 구분된 정수들의 문자열을 입력하면 런타임 예외 발생 (#8)
    * 테스트 작성
    
    * 테스트 통과하도록 구현

--- src/com/marpple/study/StringCalculator.java
+++ src/com/marpple/study/StringCalculator.java
@@ -14,6 +14,10 @@ public class StringCalculator {
             return 0;
         }
 
-        return Arrays.stream(this.expression.split(",")).mapToInt(Integer::valueOf).sum();
+        return Arrays.stream(this.expression.split(",")).mapToInt(Integer::valueOf).peek(num -> {
+            if (num < 0) {
+                throw new RuntimeException("음의 정수가 포함된 문자열은 처리할 수 없습니다.");
+            }
+        }).sum();
     }
 }
