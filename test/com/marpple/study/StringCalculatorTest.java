f9cae160f384c263fab6c4f3e3ef37cd133e056c
daengdaenglee <gunho1020@gmail.com>
2021-11-04 20:28:08 +0900

    공백 문자열 입력했을 때 테스트 작성

--- test/com/marpple/study/StringCalculatorTest.java
+++ test/com/marpple/study/StringCalculatorTest.java
@@ -2,12 +2,26 @@ package com.marpple.study;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
     @Test
     public void 빈_문자열을_입력하면_0을_반환한다() {
         String expression = "";
+        int expected = 0;
+        int actual = new StringCalculator(expression).run();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void 공백만_있는_문자열을_입력하면_0을_반환한다() {
+        int len = new Random().nextInt(1, 11);
+        char[] spaces = new char[len];
+        Arrays.fill(spaces, ' ');
+        String expression = new String(spaces);
         int expected = 0;
         int actual = new StringCalculator(expression).run();
         assertEquals(expected, actual);
