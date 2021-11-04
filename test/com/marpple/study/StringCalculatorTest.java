119f58756d383d7b0dbcbfd3e21ff58c325b37a3
daengdaenglee <gunho1020@gmail.com>
2021-11-04 20:27:36 +0900

    빈 문자열 입력했을 때 테스트 작성 및 구현

--- test/com/marpple/study/StringCalculatorTest.java
+++ test/com/marpple/study/StringCalculatorTest.java
@@ -1,7 +1,15 @@
 package com.marpple.study;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
-
+    @Test
+    public void 빈_문자열을_입력하면_0을_반환한다() {
+        String expression = "";
+        int expected = 0;
+        int actual = new StringCalculator(expression).run();
+        assertEquals(expected, actual);
+    }
 }
