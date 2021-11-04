77bce30ff7b33832ba2973360c9d20487a7db198
Kunho Lee <gunho1020@gmail.com>
2021-11-04 20:33:28 +0900

    feat: 쉼표로 구분된 음이 아닌 정수들의 문자열을 받아서 합을 계산 (#6)
    * 테스트 작성
    
    * 테스트 통과하도록 구현

--- src/com/marpple/study/StringCalculator.java
+++ src/com/marpple/study/StringCalculator.java
@@ -1,10 +1,19 @@
 package com.marpple.study;
 
+import java.util.Arrays;
+
 public class StringCalculator {
+    private final String expression;
+
     StringCalculator(String expression) {
+        this.expression = expression;
     }
 
     public int run() {
-        return 0;
+        if (this.expression.isBlank()) {
+            return 0;
+        }
+
+        return Arrays.stream(this.expression.split(",")).mapToInt(Integer::valueOf).sum();
     }
 }
