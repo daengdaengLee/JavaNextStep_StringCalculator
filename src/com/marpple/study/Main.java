a3857588636ffaf9f68b49e3a3fbb580f016c983
Kunho Lee <gunho1020@gmail.com>
2021-11-15 21:10:02 +0900

    feat: 메인 메소드 작성 (#14)
    * main 메소드 구현

--- src/com/marpple/study/Main.java
+++ src/com/marpple/study/Main.java
@@ -3,6 +3,14 @@ package com.marpple.study;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length != 1) {
+            System.out.println("잘못된 실행입니다.");
+            System.exit(1);
+            return;
+        }
+
+        StringCalculator calculator = new StringCalculator(args[0]);
+        int result = calculator.run();
+        System.out.println("결과 : " + result);
     }
 }
