aced22bb444d1ee8ec47c5126b18e55938255178
daengdaenglee <gunho1020@gmail.com>
2021-11-15 10:15:39 +0900

    세미콜론 커스텀 구분자 사용할 때 구현

--- src/com/marpple/study/StringCalculator.java
+++ src/com/marpple/study/StringCalculator.java
@@ -14,6 +14,19 @@ public class StringCalculator {
             return 0;
         }
 
+        if (this.expression.startsWith("//")) {
+            String expSep = "\n";
+            int idx = this.expression.indexOf(expSep);
+            String head = this.expression.substring(0, idx);
+            String tail = this.expression.substring(idx + expSep.length());
+            String sep = head.substring("//".length());
+            return Arrays.stream(tail.split("[" + sep + "]")).mapToInt(Integer::valueOf).peek(num -> {
+                if (num < 0) {
+                    throw new RuntimeException("음의 정수가 포함된 문자열은 처리할 수 없습니다.");
+                }
+            }).sum();
+        }
+
         return Arrays.stream(this.expression.split("[,:]")).mapToInt(Integer::valueOf).peek(num -> {
             if (num < 0) {
                 throw new RuntimeException("음의 정수가 포함된 문자열은 처리할 수 없습니다.");
