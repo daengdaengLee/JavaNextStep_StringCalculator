ea8b4aa16271dc95898b565a44217286c6cc0814
daengdaenglee <gunho1020@gmail.com>
2021-11-15 21:35:45 +0900

    Config 객체 생성자 구현

--- src/com/marpple/study/Config.java
+++ src/com/marpple/study/Config.java
@@ -1,4 +1,12 @@
 package com.marpple.study;
 
 public class Config {
+    private final String expression;
+
+    Config(String[] args) {
+        if (args.length != 1) {
+            throw new RuntimeException("잘못된 실행입니다. args 의 길이가 1 이 아닙니다.");
+        }
+        this.expression = args[0];
+    }
 }
