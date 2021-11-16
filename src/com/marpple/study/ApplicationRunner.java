46b29ba08161df739706840558b6c6070a6189b2
daengdaenglee <gunho1020@gmail.com>
2021-11-16 11:22:25 +0900

    ApplicationRunner 에서 Calculator 사용

--- src/com/marpple/study/ApplicationRunner.java
+++ src/com/marpple/study/ApplicationRunner.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ApplicationRunner {
     public static void run(Config config) {
         List<Integer> numbers = ExpressionParser.parse(config.getExpression());
-        int sum = numbers.stream().mapToInt((Integer a) -> a).sum();
+        int sum = Calculator.sumAll(numbers.stream().mapToInt((Integer a) -> a));
         System.out.println("결과 : " + sum);
     }
 }
